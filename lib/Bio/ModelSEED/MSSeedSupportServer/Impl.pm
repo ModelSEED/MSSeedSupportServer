1b2911636ee236c8f6cf07091490ced7e12b0ece
Christopher Henry <chenry@mcs.anl.gov>
2013-06-04 14:58:48 -0500

    Removing debugging print

--- lib/Bio/ModelSEED/MSSeedSupportServer/Impl.pm
+++ lib/Bio/ModelSEED/MSSeedSupportServer/Impl.pm
@@ -450,7 +450,6 @@ sub getRastGenomeData
 		}
         push(@{$output->{features}}, $newRow);
 	}
-	print Data::Dumper->Dump([$output]);
     $self->_clearContext();
     #END getRastGenomeData
     my @_bad_returns;
