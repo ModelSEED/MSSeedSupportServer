110dfc87062a1fe3adb9076cfa3b37f3b945f304
Christopher Henry <chenry@mcs.anl.gov>
2013-06-06 01:05:13 -0500

    Adding scripts to support ModelSEED

---
+++ internalScripts/BuildModelSEEDModel.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use JSON::XS;
+use Test::More;
+use Data::Dumper;
+use File::Temp qw(tempfile);
+use LWP::Simple;
+use Config::Simple;
+use Bio::KBase::fbaModelServices::Impl;
+use Bio::KBase::workspaceService::Client;
+
+$|=1;
+if (!defined($ARGV[0])) {
+	exit(0);
+}
+my $filename = $ARGV[0];
+open( my $fh, "<", $filename."jobfile.json");
+my $job;
+{
+    local $/;
+    my $str = <$fh>;
+    $job = decode_json $str;
+}
+close($fh);
+
+my $wserv = Bio::KBase::workspaceService::Client->new($job->{wsurl});
+my $fbaserv = Bio::KBase::fbaModelServices::Impl->new({accounttype => $job->{accounttype},"workspace-url" => $job->{wsurl}});
+
+#Creating the workspace if needed
+my $wsmeta;
+eval {
+	$wsmeta = $wserv->get_workspacemeta({
+		workspace => $job->{jobdata}->{owner},
+		auth => $job->{auth},
+	});
+};
+if (!defined($wsmeta)) {
+	$wserv->create_workspace({
+		workspace => $job->{jobdata}->{owner},
+		default_permission => "n",
+		auth => $job->{auth}
+	});
+}
+
+#Loading the genome if needed
+my $objmeta;
+eval {
+	$objmeta = $wserv->get_objectmeta({
+		id => $job->{jobdata}->{genome},
+		type => "Genome",
+		workspace => $job->{jobdata}->{owner},
+		auth => $job->{auth},
+	});
+};
+if (!defined($objmeta)) {
+	$fbaserv->genome_to_workspace({
+		genome => $job->{jobdata}->{genome},
+		workspace => $job->{jobdata}->{owner},
+		sourceLogin => "chenry",
+		sourcePassword => "hello824",
+		source => "rast",
+		auth => $job->{auth},
+		overwrite => 1
+	});
+}
+
+#Building model for genome
+$objmeta = undef;
+eval {
+	$objmeta = $wserv->get_objectmeta({
+		id => "Seed".$job->{jobdata}->{genome},
+		type => "Model",
+		workspace => $job->{jobdata}->{owner},
+		auth => $job->{auth},
+	});
+};
+if (!defined($objmeta)) {
+	$fbaserv->genome_to_fbamodel({
+		genome => $job->{jobdata}->{genome},
+		workspace => $job->{jobdata}->{owner},
+		model => "Seed".$job->{jobdata}->{genome},
+		auth => $job->{auth},
+	});
+}
+
+#Exporting model
+my $modeldata = $fbaserv->export_fbamodel({
+	model => "Seed".$job->{jobdata}->{genome},
+	workspace => $job->{jobdata}->{owner},
+	format => "modelseed",
+	auth => $job->{auth},
+});
+my $lines = [split(/\n/,$modeldata)];
+open(my $fh, ">", $directory."model.mdl") || return;
+open(my $fhb, ">", $directory."biomass.bof") || return;
+my $bio = 0;
+for (my $i=0; $i < @{$lines};$i++) {
+	if ($lines->[$i] =~ m/NAME\t/) {
+		$bio = 1;
+	}
+	if ($bio == 1) {
+		print $fhb $lines->[$i]."\n";
+	} else {
+		print $fh $lines->[$i]."\n";
+	}
+}
+close($fh);
+close($fhb);
+
+#Importing model
+my $cmd = "perl ModelDriver.pl mdlloadmodel Seed".$job->{jobdata}->{genome}.
+	"?".$job->{jobdata}->{genome}.
+	"?0?".$directory."model.mdl".
+	"?".$directory."biomass.bof".
+	"?".$job->{jobdata}->{owner}.
+	"?0?1";
+open(my $fh, ">", $directory."loadmodel.sh") || return;
+print $fh "sdf\nsource /homes/chenry/Model-SEED-core/bin/source-me.sh\n".$cmd."\n";
+close($fh);
+chmod 777 $directory."loadmodel.sh";
+system($directory."loadmodel.sh > ".$directory."loadmodel.out");
+
+1;
---
+++ internalScripts/QueueModelSEEDModel.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use JSON::XS;
+use Test::More;
+use Data::Dumper;
+use File::Temp qw(tempfile);
+use LWP::Simple;
+use Config::Simple;
+use Bio::KBase::fbaModelServices::Impl;
+use Bio::KBase::workspaceService::Client;
+
+my $c = Config::Simple->new();
+$c->read("~/kbase/deploy/MSQueue.ini");
+$|=1;
+if (!defined($ARGV[0])) {
+	exit(0);
+}
+my $wserv = Bio::KBase::workspaceService::Client->new($c->param("modelseed.wsurl"));
+my $owners = [];
+my $genomes = [];
+my $directory;
+if ($ARGV[0] eq "modelseed") {
+	
+} else {
+	$genomes->[0] = $ARGV[0];
+	$owners->[0] = $ARGV[1];
+	if (defined($ARGV[2])) {
+		$directory = $ARGV[2];
+	}
+}
+if (defined($directory) && -d $directory) {
+	my $JSON = JSON::XS->new();
+    my $data = $JSON->encode({
+		wsurl => $c->param("modelseed.wsurl"),
+		owner => $c->param("modelseed.owner"),
+		status => "test",
+		queuetime => DateTime->now()->datetime(),
+		id => "job.0",
+		accounttype => "seed",
+		auth => $c->param("modelseed.auth"),
+		"state" => undef,
+		type => "ModelSEED",
+		queuecommand => "QueueModelSEED",
+		jobdata => {
+			owner => $owners->[$i],
+			genome => $genomes->[$i],
+		}
+	});
+	if (-e $directory."jobfile.json") {
+		unlink $directory."jobfile.json";
+	}
+	if (-e $directory."pid") {
+		unlink $directory."pid";
+	}
+	open(my $fh, ">", $directory."jobfile.json") || return;
+	print $fh $data;
+	close($fh);
+} else {
+	for (my $i=0; $i < @{$genomes}; $i++) {
+		$wserv->queue_job({
+			auth => $c->param("modelseed.auth"),
+			"state" => undef,
+			type => "ModelSEED",
+			queuecommand => "QueueModelSEED",
+			jobdata => {
+				owner => $owners->[$i],
+				genome => $genomes->[$i],
+			}
+		});
+	}
+}
+
+1;
