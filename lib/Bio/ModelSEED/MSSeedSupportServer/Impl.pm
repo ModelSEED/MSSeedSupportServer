07bcc4306f51ddbb710ee0729df8538106c1349d
non <chenry@mcs.anl.gov>
2015-02-28 00:20:31 -0600

    Fixing bug in rast genome retrieval

--- lib/Bio/ModelSEED/MSSeedSupportServer/Impl.pm
+++ lib/Bio/ModelSEED/MSSeedSupportServer/Impl.pm
@@ -950,7 +950,7 @@ sub getRastGenomeData
 		}
         push(@{$output->{features}}, $newRow);
 	}
-	print STDERR Data::Dumper->Dump($output);
+	print STDERR Data::Dumper->Dump([$output]);
     $self->_clearContext();
     #END getRastGenomeData
     my @_bad_returns;
