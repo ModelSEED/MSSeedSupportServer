c55eab459d600587fd83cd88d14c825b6ed7c65b
non <chenry@mcs.anl.gov>
2015-03-14 21:15:55 -0500

    Fixing script

--- internalScripts/DeleteModels.pl
+++ internalScripts/DeleteModels.pl
@@ -8,7 +8,7 @@ $|=1;
 my $model = $ARGV[0];
 my $db = DBI->connect("DBI:mysql:ModelDB:bio-app-authdb.mcs.anl.gov:3306","webappuser");
 if (defined($db) && length($model) > 0) {
-	my $models = $db->selectall_arrayref("DELETE FROM ModelDB.MODEL WHERE id = ".$model);
+	my $models = $db->do("DELETE FROM ModelDB.MODEL WHERE id = ".$model);
 	$db->disconnect;
 }
 
